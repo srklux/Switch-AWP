c4a339c4a1ed7d80eefb53d9e42bd00e809f4fc9
srklux <netojoao080@gmail.com>
2024-11-27 10:10:44 -0300

    Add site

---
+++ script.js
@@ -0,0 +1,7 @@
+// O código JavaScript pode ser usado para adicionar interações dinâmicas, animações, ou funcionalidades extras
+// Nesse caso, vamos apenas incluir um exemplo simples de animação ao carregar a página
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Aqui você pode colocar animações ou interações
+    console.log("Página carregada e pronta para interações!");
+});
